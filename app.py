4051c06660b400f0524f23fc70b1482318f1c3e7
Douglas Vaz <douglas1992@gmail.com>
2015-02-18 16:07:35 +0530

    Refactored to functions

--- app.py
+++ app.py
@@ -11,51 +11,83 @@ CONSUMER_KEY = app.config['CONSUMER_KEY']
 CONSUMER_SECRET = app.config['CONSUMER_SECRET']
 CALLBACK_URL = app.config['CALLBACK_URL']
 
+def increment(n):
+	return n + 1
+
 def parse_created_at(created_at):
 	return datetime.strptime(created_at, '%a %b %d %H:%M:%S +0000 %Y')
 
-def fetch_db_docs():
+def load_db_docs():
 	client = MongoClient()
 	db = client['1self-twitter']
 	return db.docs
 
-def fetch_user_data(username):
-	docs = fetch_db_docs()
+def load_user_data(username):
+	docs = load_db_docs()
 	data = docs.find_one({"username": username})
 	return data
 
-def get_last_since_id(username):
+def load_last_since_id(username):
 	id = 1
-	data = fetch_user_data(username)
+	data = load_user_data(username)
 	if data != None and 'since_id' in data:
 		id = data['since_id']
 	return id
 
-def set_last_since_id(username, id):
-	doc = fetch_user_data(username)
+def save_last_since_id(username, id):
+	doc = load_user_data(username)
 	if doc == None:
 		doc = {}
 	doc["username"] = username
 	doc["since_id"] = id
 	doc["datetime"] = datetime.utcnow()
-	return docs.update({"username": username}, doc, upsert=True)
+	return load_db_docs().update({"username": username}, doc, upsert=True)
 
-def fetch_oauth_tokens(username):
-	data = fetch_user_data(username)
+def load_oauth_tokens(username):
+	data = load_user_data(username)
 	if data != None and 'oauth_token' in data and 'oauth_token_secret' in data:
 		return data['oauth_token'], data['oauth_token_secret']
 	else:
 		return None
 
 def save_ouath_token(username, oauth_token, oauth_token_secret):
-	doc = fetch_user_data(username)
+	doc = load_user_data(username)
 	if doc == None:
 		doc = {}
 	doc["username"] = username
 	doc["oauth_token"] = oauth_token
 	doc["oauth_token_secret"] = oauth_token_secret
 	doc["datetime"] = datetime.utcnow()
-	return fetch_db_docs().update({"username": username}, doc, upsert=True)
+	return load_db_docs().update({"username": username}, doc, upsert=True)
+
+def client_factory(key, secret, access_token=None, access_secret=None):
+	if access_token is not None and access_secret is not None:
+		return UserClient(key, secret, access_token, access_secret)
+	else:
+		return UserClient(key, secret)
+
+def fetch_client_username(client):
+	return client.api.account.settings.get().data['screen_name']
+
+def fetch_client_tweets(client, since_id=1):
+	return client.api.statuses.user_timeline.get(since_id=since_id)
+
+def generate_tweet_counts(tweets):
+	dates = [parse_created_at(tweet.created_at).date() for tweet in tweets.data]
+	counts = {}
+	for date in dates:
+		if date in counts:
+			counts[date] = increment(counts[date])
+		else:
+			counts[date] = 1
+	return counts
+
+def get_max_tweetId(tweets):
+	ids = [int(tweet.id) for tweet in tweets.data]
+	if len(ids) > 0:
+		return max(ids)
+	else:
+		return 1
 
 def register_stream():
 	url = app.config['API_URL'] + "/v1/streams"
@@ -102,16 +134,11 @@ def send_batch_events(events, stream):
 def build_graph_url(stream):
 	objectTags = app.config['OBJECT_TAGS']
 	actionTags = app.config['ACTION_TAGS']
-
 	def strigify_tags(tags):
 		return str(",".join(tags))
 
 	url = app.config['API_URL'] + u"/v1/streams/" + stream['streamid'] + "/events/tweets/tweet/sum(count)/daily/barchart?readToken=" + stream['readToken'] + "&bgColor=00acee";
-
 	return url
-
-def increment(n):
-	return n + 1
 
 @app.route("/")
 def index():
@@ -119,7 +146,7 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	client = UserClient(CONSUMER_KEY, CONSUMER_SECRET)
+	client = client_factory(CONSUMER_KEY, CONSUMER_SECRET)
 	token = client.get_signin_token(CALLBACK_URL)
 	app.config['ACCESS_TOKEN'] = token.oauth_token
 	app.config['ACCESS_TOKEN_SECRET'] = token.oauth_token_secret
@@ -128,29 +155,24 @@ def login():
 @app.route('/callback')
 def callback():
 	OAUTH_VERIFIER = request.args.get('oauth_verifier')
-	client = UserClient(CONSUMER_KEY, CONSUMER_SECRET,
+	client = client_factory(CONSUMER_KEY, CONSUMER_SECRET,
 		app.config['ACCESS_TOKEN'], app.config['ACCESS_TOKEN_SECRET'])
 	token = client.get_access_token(OAUTH_VERIFIER)
 
-	username = client.api.account.settings.get().data['screen_name']
+	username = fetch_client_username(client)
 	save_ouath_token(username, token.oauth_token, token.oauth_token_secret)
 	
-	response = client.api.statuses.user_timeline.get(since_id=get_last_since_id(username))
-	dates = [parse_created_at(tweet.created_at).date() for tweet in response.data]
-	ids = [int(tweet.id) for tweet in response.data]
-	if len(ids) > 0:
-		set_last_since_id(username, max(ids))
+	tweets = fetch_client_tweets(client, load_last_since_id(username))
 
-	counts = {}
-	for date in dates:
-		if date in counts:
-			counts[date] = increment(counts[date])
-		else:
-			counts[date] = 1
+	####UPDATE LAST TWEET ID!
+	sync_id = get_max_tweetId(tweets)
+	save_last_since_id(username, sync_id)
 
+	counts = generate_tweet_counts(tweets)
 	stream, status = register_stream()
-	print(send_batch_events(build_events(counts), stream))
-	print(fetch_oauth_tokens(username))
+	send_batch_events(build_events(counts), stream)
+	
+	print(load_oauth_tokens(username))
 
 	return render_template("tweets.html", counts=counts, username=username, url=build_graph_url(stream))
 
