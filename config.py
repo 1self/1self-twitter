74973129240a14475b165883651a841bda729e68
Chinmay Naik <chinmay185@gmail.com>
2015-02-27 11:24:43 +0530

    add APP_URL

--- config.py
+++ config.py
@@ -35,6 +35,7 @@ ACTION_TAGS = ["tweet"]
 OBJECT_TAGS = ["tweets"]
 
 API_URL = os.getenv('API_URL')
+APP_URL = os.getenv('APP_URL')
 APP_ID = os.getenv('APP_ID')
 APP_SECRET = os.getenv('APP_SECRET')
 
